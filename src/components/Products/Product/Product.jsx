78388b1cd3380087e279df37d787376fb62c88c7
Chris Patrick <atomize101@gmail.com>
2020-12-01 13:08:17 -0500

    Adding the Product and Products components

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = ({ product }) => {
+	return (
+		<div>
+			<Card className={classes.root}>
+				<CardMedia classes={classes.media} image="" title={product.name} />
+				<CardContent>
+					<div className={classes.cardContent}>
+						<Typography variant="h5" gutterBottom>
+							{product.name}
+						</Typography>
+						<Typography variant="h5" gutterBottom>
+							{product.price}
+						</Typography>
+						<Typography variant="h2" color="textSecondary">
+							{product.description}
+						</Typography>
+					</div>
+				</CardContent>
+				<CardActions disableSpacing className={classes.cardActions}>
+					<IconButton aria-label="Add to Cart">
+						<AddShoppingCart />
+					</IconButton>
+				</CardActions>
+			</Card>
+		</div>
+	);
+};
+
+export default Product;
