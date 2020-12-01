ed7656f046cc3868eb1a210295d1d517ea30fce8
Chris Patrick <atomize101@gmail.com>
2020-12-01 13:41:54 -0500

    Products cards now display

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+import Product from './Product/Product';
 
 const products = [
 	{
 		id: 1,
 		name: 'Shoes',
 		description: 'Running Shoes',
-		price: '$10'
+		price: '$10',
 	},
 	{
 		id: 2,
 		name: 'Macbook',
 		description: 'Apple Macbook',
-		price: '$800'
+		price: '$800',
 	},
 ];
 
 const Products = () => {
-	<main>
-		<Grid container justify="center" spacing={4}>
-           {products.map((product) => (
-               <Grid item key={product.id} xs={12} sn={6} md={4} lg={3}>
-                   <Product product={product} />
-               </Grid>
-           )} 
-        </Grid>
-	</main>;
+	return (
+		<main>
+			<Grid container justify="center" spacing={4}>
+				{products.map((product) => (
+					<Grid item key={product.id} xs={12} sn={6} md={4} lg={3}>
+						<Product product={product} />
+					</Grid>
+				))}
+			</Grid>
+		</main>
+	);
 };
 
 export default Products;
