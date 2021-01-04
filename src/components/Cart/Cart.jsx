e628383cc8249a70c4b2de2057267a763a7a2252
Chris Patrick <atomize101@gmail.com>
2021-01-04 13:11:33 -0500

    Cart now displaying

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
 const Cart = ({ cart }) => {
 	const classes = useStyles();
 
-	const isEmpty = true;
+	if (!cart.line_items) return 'Loading';
 
-	const EmptyCart = () => {
-		return <Typography variant="subtitle1">You have no items in your shopping cart, start adding some</Typography>;
-	};
+	const renderEmptyCart = () => (
+		<Typography variant="subtitle1">
+			You have no items in your shopping cart,
+			<Link className={classes.link} to="/">
+				start adding some
+			</Link>
+			!
+		</Typography>
+	);
 
 	const FilledCart = () => {
-		<>
-			<Grid container spacing={3}></Grid>
-		</>;
+		return (
+			<>
+				<Grid container spacing={3}>
+					{cart.line_items.map((lineItem) => (
+						<Grid item xs={12} sm={4} key={lineItem.id}>
+							<div>{lineItem.name}</div>
+						</Grid>
+					))}
+				</Grid>
+				<div className={classes.cartDetails}>
+					<Typography varian="h4">Subotal: {cart.subtotal.formatted_with_symbol}</Typography>
+					<div>
+						<Button
+							className={classes.emptyButton}
+							size="large"
+							type="button"
+							variant="contained"
+							color="secondary"
+						>
+							Empty Cart
+						</Button>
+						<Button
+							className={classes.checkoutButton}
+							size="large"
+							type="button"
+							variant="contained"
+							color="primary"
+						>
+							Checkout
+						</Button>
+					</div>
+				</div>
+			</>
+		);
 	};
 
 	return (
@@ -24,7 +62,7 @@ const Cart = ({ cart }) => {
 			<Typography className={classes.title} variant="h3">
 				Your shopping cart
 			</Typography>
-			{isEmpty ? <EmptyCart /> : <FilledCart />}
+			{!cart.line_items.length ? renderEmptyCart() : FilledCart()}
 		</Container>
 	);
 };
