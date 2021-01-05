b20676db629645605b795e8c31041cb4c5f7a487
Chris Patrick <atomize101@gmail.com>
2021-01-05 12:15:56 -0500

    Wiring up CartItem with Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
 	const classes = useStyles();
@@ -23,7 +24,7 @@ const Cart = ({ cart }) => {
 				<Grid container spacing={3}>
 					{cart.line_items.map((lineItem) => (
 						<Grid item xs={12} sm={4} key={lineItem.id}>
-							<div>{lineItem.name}</div>
+							<CartItem item={lineItem} />
 						</Grid>
 					))}
 				</Grid>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import useStyles from './styles';
 
-const CartItem = () => {
-	return <div></div>;
+const CartItem = ({ item }) => {
+	const classes = useStyles();
+
+	return (
+		<Card>
+			<CardMedia image={item.media.source} alt={item.name} />
+		</Card>
+	);
 };
 
 export default CartItem;
